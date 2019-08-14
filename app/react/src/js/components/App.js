3c1d05760160f264c55ae9170aabc0865b719cdd
Daniel Páez <danielpaezsw66@gmail.com>
2019-08-14 18:54:19 -0400

    Adding webpack add with react config

---
+++ app/react/src/js/components/App.js
@@ -0,0 +1,21 @@
+
+import React from "react";
+import LoaderList from "./LoaderList.js"
+
+
+function App(props) {
+
+	return(
+		<section className="App">
+			
+			<h1>App Testing</h1>
+
+			<LoaderList />
+
+		</section>
+	)
+}
+
+export default App;
+
+
